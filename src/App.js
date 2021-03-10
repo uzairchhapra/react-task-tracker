673e79df203919776ce15061f1149909334cf542
Uzair <uzair.chhapra@deprontoinfoctech.com>
2021-03-10 21:02:12 +0530

    tasks delete using filter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import { useState } from 'react'
+
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'A',
+      day: 1,
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'B',
+      day: 2,
+      reminder: false,
+    },
+    {
+      id: 3,
+      text: 'C',
+      day: 3,
+      reminder: true,
+    },
+  ]);
+
+  //Delete Task
+  const deleteTask = (id) => {
+    console.log('delete ', id);
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
   return (
     <div className="container">
       <Header title='Task Tracker' />
-      <Tasks />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'No tasks to show'}
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,30 +1,11 @@
-const tasks = [
-    {
-        id: 1,
-        text: 'A',
-        day: 1,
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'B',
-        day: 2,
-        reminder: false,
-    },
-    {
-        id: 3,
-        text: 'C',
-        day: 3,
-        reminder: true,
-    },
-];
+import Task from './Task';
 
-const Tasks = () => {
-    console.table(tasks);
+const Tasks = ({ tasks, onDelete }) => {
+    // console.table(tasks);
     return (
         <>
             {tasks.map((task) => (
-                <h3 key={task.id}>{task.text}</h3>
+                <Task key={task.id} task={task} onDelete={onDelete} />
             ))}
         </>
     )
