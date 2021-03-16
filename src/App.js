9bb773d8eac8acdf7afde151235855aa47a246d9
Uzair <uzair.chhapra@deprontoinfoctech.com>
2021-03-16 16:02:53 +0530

    addTasks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 import { useState } from 'react'
 
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
+
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -25,16 +28,31 @@ function App() {
     },
   ]);
 
+  //AddTask
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 1000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+    console.log(task);
+  }
+
   //Delete Task
   const deleteTask = (id) => {
     console.log('delete ', id);
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    console.log('toggle', id);
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task));
+  }
+
   return (
     <div className="container">
-      <Header title='Task Tracker' />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'No tasks to show'}
+      <Header title='Task Tracker' onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No tasks to show'}
     </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 
 const Button = ({ color, text, onClick }) => {
     return (
-        <button onClick={onClick} className='btn' style={{ backgroundColor: color }}>Add</button>
+        <button onClick={onClick} className='btn' style={{ backgroundColor: color }}>{text}</button>
     )
 }
 
